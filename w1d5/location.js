435e7cda9e566085fdc47ff526e5865e8d6bc9f4
chris <pachopa0320@gmail.com>
2017-05-08 15:22:03 +0000

    index.js

---
+++ w1d5/location.js
@@ -0,0 +1,46 @@
+module.exports = {
+  X: 128 ,
+  hello: function(""){
+    console.log("The location of the lighthouse labs is " + this.X);
+  }
+};
+
+// module.exports = {
+//   PHI: 1.618,
+//   explain: function() {
+//     console.log("(X+Y) is to X as X is to Y: (X+Y)≈" + this.PHI);
+//   }
+// };
+
+
+
+
+
+
+
+// var main.js = require("./location");
+
+// console.log(main.X);
+
+// main.explain();
+
+// var coolMath = require("./math-is-cool");
+
+// console.log(coolMath.PHI);  // log a Number
+
+// coolMath.explain();  // invokes the function `explain`
+
+
+
+
+
+
+// var coolMath = require("./math-is-cool");
+
+// console.log(coolMath.PHI);  // log a Number
+
+// coolMath.explain();  // invokes the function `explain`
+
+
+
+
---
+++ w1d5/main.js
@@ -0,0 +1,34 @@
+
+var main = require("./location");
+
+console.log(main.hello(""));
+
+
+
+
+
+// var coolMath = require("./math-is-cool");
+
+// console.log(coolMath.PHI);  // log a Number
+
+// coolMath.explain();  // invokes the function `explain`
+
+
+
+
+
+
+
+// var summedResult = sumUp(process.argv);
+// console.log(summedResult);
+// Then we can have another file called sum.js
+
+// function sumUp(arr){
+//   var sum = 0;
+
+//   for(var i = 0; i < arr.length; i += 1){
+//     if(!isNaN(arr[i])){
+//       sum += Number(arr[i]);
+//     }
+//   }
+//   return sum;
